78b97025264cc602a50d4c224d705722e02c7a09
Alcides Ojeda <eojedapilchik@gmail.com>
2021-05-06 10:51:24 -0400

    added filter responded

--- js/app.js
+++ js/app.js
@@ -2,7 +2,36 @@
 
 const form = document.getElementById("registrar");
 const input = form.querySelector("input");
+const mainDiv = document.querySelector(".main");
 const ul = document.getElementById("invitedList");
+
+const div = document.createElement("div");
+const filterLabel = document.createElement("label");
+const filterCheckBox = document.createElement("input");
+
+filterLabel.textContent = "Hide those who haven't responded";
+filterCheckBox.type = "checkbox";
+div.appendChild(filterLabel);
+div.appendChild(filterCheckBox);
+mainDiv.insertBefore(div, ul);
+
+filterCheckBox.addEventListener("change", (e) => {
+  const isChecked = e.target.checked;
+  const listInvitees = ul.children;
+  if (isChecked) {
+    for (const invitee of listInvitees) {
+      if (invitee.className === "responded") {
+        invitee.style.display = "";
+      } else {
+        invitee.style.display = "none";
+      }
+    }
+  } else {
+    for (const invitee of listInvitees) {
+      invitee.style.display = "";
+    }
+  }
+});
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -92,7 +121,6 @@ function editStateListItem(li, button) {
  * @param {element} li - list item to put in edit state
  * @param {button} button - edit button
  */
-
 function saveStateListItem(li, button) {
   const span = document.createElement("span");
   const input = li.firstElementChild;
