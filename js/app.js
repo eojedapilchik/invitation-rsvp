020a7a394a0fb92dad512b4c0a1456e1b1c0c09d
Alcides Ojeda <eojedapilchik@gmail.com>
2021-05-06 08:57:23 -0400

    added confirmed to items

--- js/app.js
+++ js/app.js
@@ -5,13 +5,26 @@ const ul = document.getElementById("invitedList");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   createListItem();
-  //Clears the input text
-  //   console.log(input.value);
 });
 
 function createListItem() {
   const li = document.createElement("li");
   li.textContent = input.value;
+  const label = document.createElement("label");
+  label.textContent = "Confirmed";
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  label.appendChild(checkbox);
+  li.appendChild(label);
   ul.appendChild(li);
   input.value = "";
 }
+
+// to handle the event of the change - confirmed checkbox-
+ul.addEventListener("change", (e) => {
+  const checkbox = e.target;
+  const checked = checkbox.checked;
+  const listItem = checkbox.parentNode.parentNode; // first parent is the label, seconf parent is the Li
+  checked ? (listItem.className = "responded") : (listItem.className = "");
+  //e.target.tagName == "INPUT"
+});
